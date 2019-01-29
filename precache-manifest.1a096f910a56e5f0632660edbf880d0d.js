f67297b33cf209fdb44cf7a699d2f1d2178d61b8
Francis Amankrah <francis.amankrah@ppksgroup.com>
2019-01-28 19:49:49 -0500

    Deployed to Github pages

---
+++ precache-manifest.1a096f910a56e5f0632660edbf880d0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b65718b2ac20af5e0821",
+    "url": "/static/js/main.b65718b2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b65718b2ac20af5e0821",
+    "url": "/static/css/main.22dcdaa4.chunk.css"
+  },
+  {
+    "revision": "241458cadfd1094ce261d65558d5eb29",
+    "url": "/index.html"
+  }
+];
