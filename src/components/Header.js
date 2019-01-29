edb5ef08cbf5fd166ac8483f68beb16de91663fc
Francis Amankrah <francis.amankrah@ppksgroup.com>
2019-01-29 09:46:48 -0500

    Added some assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+import namesSrc from '../assets/JandF.png';
+import dateSrc from '../assets/date.png';
+import lineSrc from '../assets/line.png';
+
 import type { PageType } from '../constants';
 
 type Props = {
@@ -12,26 +16,46 @@ type Props = {
 export default (props: Props) => {
   return (
     <Wrapper>
-      {menuLinks.map(menu => (
-        <MenuLink key={menu[0]} to={menu[0]}>{menu[1]}</MenuLink>
-      ))}
+      <MenuLink to="/invitation">Invitation</MenuLink>
+      <MenuLink to="/city">Montreal</MenuLink>
+
+      <JayneFrankWrapper>
+        <Image height="50px" image={namesSrc} />
+        <Image height="20px" image={dateSrc} />
+        <Image height="2px" image={lineSrc} />
+      </JayneFrankWrapper>
+
+      <MenuLink to="/photos">Photos</MenuLink>
+      <MenuLink to="/facts">Fun Facts</MenuLink>
     </Wrapper>
   );
 }
 
-const menuLinks = [
-  ['/invitation', 'Invitation'],
-  ['/city', 'Montreal'],
-  ['/photos', 'Photos'],
-  ['/facts', 'Fun Facts'],
-];
-
 const Wrapper = styled.header`
   background-color: white;
+  padding: 2vw;
+
   display: flex;
   justify-content: space-around;
+  align-items: flex-end;
 `;
 
 const MenuLink = styled(Link)`
   text-transform: uppercase;
+`;
+
+// Jayne & Frank
+const JayneFrankWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 300px;
+`;
+
+const Image = styled.div`
+  background: transparent center center no-repeat url('${props => props.image}');
+  background-size: contain;
+  height: ${props => props.height};
+  margin: 10px auto;
+  width: 100%;
 `;
