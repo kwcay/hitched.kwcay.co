0ae74df2da0ef746ab70cb83220dc9166c483083
Francis Amankrah <francis.amankrah@ppksgroup.com>
2019-01-31 09:53:50 -0500

    Added frame to landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,11 @@
 // @flow
 import React from 'react';
+import styled from 'styled-components';
 
 import type { ContextRouter } from 'react-router-dom';
 
 import Header from '../components/Header';
+import frameSrc from '../assets/frame.png';
 import MainWrapper from '../components/MainWrapper';
 
 import * as constants from '../constants';
@@ -13,7 +15,17 @@ export default (props: ContextRouter) => (
     <Header route={constants.LANDING_ROUTE} />
 
     <MainWrapper>
-      English / Fran&ccedil;ais
+      <Frame>
+        English / Fran&ccedil;ais
+      </Frame>
     </MainWrapper>
   </React.Fragment>
 );
+
+const Frame = styled.div`
+  background: transparent center center no-repeat url('${frameSrc}');
+  background-size: contain;
+  width: 80vw;
+  height: 65vw;
+  margin: 0px auto;
+`;
