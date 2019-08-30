654d65c46f37bc752781c11cbb804c1f1ca9929c
frnkly <frank@frnk.ca>
2019-08-29 22:01:07 -0400

    Deployed to Github pages

---
+++ precache-manifest.d97b35b7c7243025997b62622041e93e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18d55a0eda0bb4fc33d62178b2a7948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2fdc6c14adf162d4f6f",
+    "url": "/static/css/main.472783f4.chunk.css"
+  },
+  {
+    "revision": "0eb6307118cbf6db9a4e",
+    "url": "/static/js/2.9bf05247.chunk.js"
+  },
+  {
+    "revision": "e2fdc6c14adf162d4f6f",
+    "url": "/static/js/main.a5ff003e.chunk.js"
+  },
+  {
+    "revision": "f9b368003e36b2906b1c",
+    "url": "/static/js/runtime~main.0c21802f.js"
+  },
+  {
+    "revision": "3ffe021f8cab157c229d6a336eb15fe1",
+    "url": "/static/media/JandF.3ffe021f.png"
+  },
+  {
+    "revision": "70e0d367379f141dcd10cf2fdc00b660",
+    "url": "/static/media/background.70e0d367.jpg"
+  },
+  {
+    "revision": "baaba2feaf817fa33d17b4555c75b531",
+    "url": "/static/media/cover.desktop.baaba2fe.png"
+  },
+  {
+    "revision": "7dd3d2f7e2d1829c83cfb8839e89d413",
+    "url": "/static/media/cover.tablet.7dd3d2f7.png"
+  },
+  {
+    "revision": "9a744a66e40847eba7b56728fd4ad928",
+    "url": "/static/media/date.9a744a66.png"
+  },
+  {
+    "revision": "efc06e6ebf2592f6f25155d3dc1f8dc8",
+    "url": "/static/media/frame.desktop.efc06e6e.png"
+  },
+  {
+    "revision": "dfbf4b598774f48dcc5831eaefe8e5f1",
+    "url": "/static/media/frame.phone.dfbf4b59.png"
+  },
+  {
+    "revision": "088913fadb7e4448d7dae236880b7285",
+    "url": "/static/media/frame.tablet.088913fa.png"
+  },
+  {
+    "revision": "73ce7e5d05022d6df57db9d0f6fde8c1",
+    "url": "/static/media/texture.desktop.73ce7e5d.png"
+  },
+  {
+    "revision": "9110613f99c390ffe9a9ea7297dbfcaf",
+    "url": "/static/media/texture.phone.9110613f.png"
+  },
+  {
+    "revision": "f84932eb324a3fd0da64948ca488940a",
+    "url": "/static/media/texture.tablet.f84932eb.png"
+  }
+]);
